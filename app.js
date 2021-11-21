70b3bb4bd0c4f932c8544178713a9c292d5c14fe
lean9ema <lean9marino@gmail.com>
2021-11-20 23:40:39 -0300

    Agregue las nuevas rutas para los carritos de compras

--- app.js
+++ app.js
@@ -16,9 +16,6 @@ app.get('/login', (req,res) => {
     res.sendFile(__dirname + '/views/login.html');
 })
 
-app.get('/productCart', (req,res) => {
-    res.sendFile(__dirname + '/views/productCart.html');
-})
 
 app.get('/productDetail', (req,res) => {
     res.sendFile(__dirname + '/views/productDetail.html');
@@ -29,3 +26,18 @@ app.get('/register', (req,res) => {
     res.sendFile(__dirname + '/views/register.html');
 })
 
+app.get('/productCart', (req,res) => {
+    res.sendFile(__dirname + '/views/productCart.html');
+})
+
+app.get('/productCart2', (req,res) => {
+    res.sendFile(__dirname + '/views/productCart.html');
+})
+
+app.get('/productCart3', (req,res) => {
+    res.sendFile(__dirname + '/views/productCart.html');
+})
+
+app.get('/productCart4', (req,res) => {
+    res.sendFile(__dirname + '/views/productCart.html');
+})
